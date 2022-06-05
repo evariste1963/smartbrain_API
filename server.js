7b2513c2787e19bbb66366aa819094719edac102
evariste1963 <evariste@hotmail.co.uk>
2022-06-05 18:49:12 +0100

    all endpoints done --> refactoring required

--- server.js
+++ server.js
@@ -68,14 +68,21 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json("profile not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
-
-/*
-/--> res => this is working
-/signin --> POST = sucess/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-*/
