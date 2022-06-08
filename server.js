07bd07f74b214bbab72771fccf01408d206ad156
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-06-08 10:09:11 +0100

    increment id corrected -- result back to string

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post("/signin", (req, res) => {
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
   database.users.push({
-    id: +database.users.at(-1).id + 1,
+    id: (+database.users.at(-1).id + 1).toString(),
     name: name,
     email: email,
     password: password,
